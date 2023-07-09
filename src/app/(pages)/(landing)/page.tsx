6f11f060871b46ca333cac3c9b5741e515ae3a48
willie m <zx10rwillie@gmail.com>
2023-07-08 21:52:14 -0500

    Updating style's almost done.

--- src/app/(pages)/(landing)/page.tsx
+++ src/app/(pages)/(landing)/page.tsx
@@ -6,11 +6,21 @@ import healthcare from '../../../../public/svg/healthcare.svg'
 import daycare from '../../../../public/svg/daycare.svg'
 import training from '../../../../public/svg/training.svg'
 import hyginic from '../../../../public/svg/hyginic.svg'
+import sheepdog from '../../../../public/images/sheepdog.png'
+import bone from '../../../../public/svg/Group.svg'
+import bone1 from '../../../../public/svg/Group-1.svg'
+import bone2 from '../../../../public/svg/Group-2.svg'
+import bone3 from '../../../../public/svg/Group-3.svg'
+import food1 from '../../../../public/images/food1.png'
+import food2 from '../../../../public/images/food2.png'
+import food3 from '../../../../public/images/food3.png'
+import walking from '../../../../public/images/walking.png'
+
 
 export default function Home() {
   return (
     
-    <main className=" ">
+    <main >
       <section className=' mt-48 mb-64'>
         <Image
           src={vec}
@@ -79,10 +89,129 @@ export default function Home() {
           </li>
         </ul>
       </section>
-         
-       
-    
-
+      <section className='flex flex-row justify-center'>
+        <div className="LeftImg w-[761.17px] h-[750.73px] relative mr-48">
+          <Image className="ShetlandSheepdogLyingFrontWhiteWall19197119183RemovebgPreview1 w-[589px] h-[424px] left-[144px] top-[134.88px] absolute" alt='sheepdog' src={sheepdog} />
+          <Image src={bone} alt='bone' className="Group origin-top-left rotate-[137.13deg] w-[37.85px] h-[41.99px] left-[606.81px] top-[302.11px] absolute"/>
+          <Image src={bone1} alt='bone' className="Group origin-top-left rotate-[72.97deg] w-[37.85px] h-[41.99px] left-[237.34px] top-[264.88px] absolute"/>
+          <Image src={bone2} alt='bone' className="Group origin-top-left rotate-[104deg] w-[37.81px] h-[41.94px] left-[419.34px] top-[123.36px] absolute"/>
+          <Image src={bone3} alt='bone' className="Group origin-top-left rotate-[-22.93deg] w-[42.21px] h-[29.59px] left-[221.46px] top-[391.16px] absolute"/>
+        </div>
+        <div className=' my-48'>
+          <h2 className=' mb-16 text-4xl font-bold w-[510px]'>
+            Dogs do speak, but only to <br /> those who know how to <br /> listen.
+          </h2>
+          <p className=' mb-16 text-2xl w-[510px]'>Sweet roll ice cream powder candy canes ice cream donut pudding biscuit ice cream.
+            Cupcake tootsie roll sugar plum danish pudding fruitcake cheesecake jelly-o. Pie muffin topping cake. 
+            Pudding biscuit caramels topb
+            </p>
+          <button className=' w-48 h-16 bg-cyan-900 text-white font-semibold rounded-lg'>EXPLORE MORE</button>
+        </div>
+      </section>
+      <section className=' mx-80 '>
+        <h2 className='text-3xl font-mediu'>Dog Nutrients & Food</h2>
+        <p className='mb-20 border-b-4 border-cyan-600 w-1/12'></p>
+        <div className='flex flex-row '>
+          <h3 className='text-6xl font-bold mb-20'>25% OFF ALL PRODUCTS</h3>
+          <button className=' ml-[450px] w-[192.69px] h-[59.60px] bg-cyan-900 rounded text-white'>View More</button>
+        </div>
+        <div>
+          <ul className='flex flex-row text-center justify-between'>
+            <li className='flex flex-col px-11'>
+              <Image src={food1} alt='dog food' className='mx-auto mb-10'/>
+              <h4 className=' text-2xl'>Drools | 3KG</h4>
+              <p>Adult chicken and egg Egg, Chicken 3 kg Dry Adult dig Food</p>
+              <button className=" ml-24 mt-10 w-[187px] h-[52px] rounded-md border-[3px] text-cyan-900 font-semibold text-[20px] border-cyan-900">Buy Now</button>
+            </li>
+            <li className='flex flex-col px-11'>
+              <Image src={food2} alt='dog food' className='mx-auto mb-10'/>
+              <h4 className=' text-2xl'>Canine Creek 4 KG</h4>
+              <p>Adult chicken and egg Egg, Chicken 3 kg Dry Adult dig Food</p>
+              <button className=" ml-24 mt-10 w-[187px] h-[52px] rounded-md border-[3px] text-cyan-900 font-semibold text-[20px] border-cyan-900">Buy Now</button>
+            </li>
+            <li className='flex flex-col px-11'>
+              <Image src={food3} alt='dog food' className='mx-auto mb-10'/>
+              <h4 className=' text-2xl'>Biscork Biscuits</h4>
+              <p>Adult chicken and egg Egg, Chicken 3 kg Dry Adult dig Food</p>
+              <button className=" ml-24 mt-10 w-[187px] h-[52px] rounded-md border-[3px] text-cyan-900 font-semibold text-[20px] border-cyan-900" >Buy Now</button>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className='flex flex-row justify-around mx-80'>
+        <div className='mt-48'>
+          <h2 className='mb-16 text-4xl font-bold w-[508px]'>NO ONE APPRECIATES THE VERY SPECIAL GENIUS OF YOUR CONVERSATION AS THE DOG DOES.</h2>
+          <p className='mb-16 text-2xl w-[533px]'>Sweet roll ice powder candy canes ice cream donut pudding buscuit ice cream biscuit topb</p>
+          <button className='w-[192.69px] h-[59.60px] bg-cyan-900 rounded text-white'>Explore More</button>
+        </div>
+        <div>
+          <Image src={walking} alt='walking a dog' className='mt-28'/>
+        </div>
+      </section>
+      <section className='mx-80 flex flex-col'>
+        <h2 className='text-3xl font-mediu'>Happy Customer</h2>
+        <p className='mb-20 border-b-4 border-cyan-600 w-1/12'></p>
+        <ul className='flex flex-row justify-between text-center'>
+          <li>
+            <div>
+              <Image src={training} alt='its a dog' className='mx-auto'/>
+            </div>
+            <div className='bg-cyan-900'>
+              <h3 className='text-amber-300'>Anna & Tobby</h3>
+              <p className='text-white'>Amazing Products & Delivery on time.</p>
+              <p>bones</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Image src={training} alt='its a dog' className='mx-auto'/>
+            </div>
+            <div className='bg-cyan-900'>
+              <h3 className='text-amber-300'>Anna & Tobby</h3>
+              <p className='text-white'>Amazing Products & Delivery on time.</p>
+              <p>bones</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Image src={training} alt='its a dog' className='mx-auto'/>
+            </div>
+            <div className='bg-cyan-900'>
+              <h3 className='text-amber-300'>Anna & Tobby</h3>
+              <p className='text-white'>Amazing Products & Delivery on time.</p>
+              <p>bones</p>
+            </div>
+          </li>
+        </ul>
+        <button className='w-[192.69px] h-[59.60px] bg-cyan-900 rounded text-white mx-auto my-20'>Explore More</button>
+        
+      </section>
+      <section>
+        <div>
+          <h2>JOIN US WITH AFFILIATE PROGRAM</h2>
+          <div>
+            <input type="text" />
+            <button className='w-[192.69px] h-[59.60px] bg-cyan-900 rounded text-white'>Submit now</button>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <h3>Follow on social</h3>
+            <p>svg</p>
+            <p>svg</p>
+          </li>
+          <li>
+            <h3>Boarding &day care</h3>
+            <p>(616)-459-8622</p>
+            <p>3236 Howard Street ,Grand <br /> Rapids,MI Michigan</p>
+          </li>
+          <li>
+            <h3>Health care</h3>
+            <p>(616)-556-5896</p>
+            <p>3236 Howard Street ,Grand <br /> Rapids,MI Michigan</p>
+          </li>
+        </ul>
+      </section>
     </main>
   )
 }
--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className=' flex flex-row px-80 mb-8 pt-8 border-t-4 border-trueGray-300'>
+      <p>all rights reserved & design by bharat</p>
+      <Link href='/' className=' font-bold text-lg mr-10 ml-auto'>OUR SERVICES</Link>
+      <Link href='/' className=' font-bold text-lg mr-10'>ABOUT US</Link>
+      <Link href='/' className=' font-bold text-lg mr-10'>SHIPPING</Link>
+      <Link href='/' className=' font-bold text-lg mr-10'>FAQ</Link>
+      <Link href='/' className=' font-bold text-lg mr-10'>CONTACT</Link>
+    </div>
   )
 }
