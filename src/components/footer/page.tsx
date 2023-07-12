2e7cbd0b95fc9e8e796758e0e6fbb4e91685976b
willie m <zx10rwillie@gmail.com>
2023-07-11 20:19:33 -0500

    fixed the mis aligned items and added a max screen,so the lg srenn won't breake when you go over 1440p.

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div className=' flex flex-col text-center xl:flex-row xl:px-80 mb-8 pt-8 border-t-4 border-trueGray-300'>
+    <div className=' flex flex-col text-center xl:flex-row xl:px-40 mb-8 pt-8 border-t-4 border-trueGray-300  max-w-[1440px] mx-auto'>
       <p>all rights reserved & design by bharat</p>
       <Link href='/' className=' font-bold text-lg my-2 xl:my-0 xl:mr-10 xl:ml-auto'>OUR SERVICES</Link>
       <Link href='/' className=' font-bold text-lg my-2 xl:my-0 xl:mr-10'>ABOUT US</Link>
